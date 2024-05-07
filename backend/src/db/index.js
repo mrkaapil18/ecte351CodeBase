7c14ddfc2a77c0ca6757c7ff511d2f3dac842657
mrkaapil18 <kv471@uowmail.edu.au>
2024-05-07 17:36:27 +1000

    added the mongoose Connection

---
+++ backend/src/db/index.js
@@ -0,0 +1,16 @@
+import { mongoose } from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.host}`);
+  } catch (error) {
+    console.log("MONGO_DB connection Failed", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,66 +1,91 @@
-import express from 'express';
+import dotenv from "dotenv";
+
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
 
 const app = express();
 
-app.get('/api/admin/test',(req,res) => {
-    const arrayOfObjects = [
-        {
-            id: 1,
-            title: "First Object",
-            content: "This is the content of the first object."
-        },
-        {
-            id: 2,
-            title: "Second Object",
-            content: "This is the content of the second object."
-        },
-        {
-            id: 3,
-            title: "Third Object",
-            content: "This is the content of the third object."
-        },
-        {
-            id: 4,
-            title: "Fourth Object",
-            content: "This is the content of the fourth object."
-        },
-        {
-            id: 5,
-            title: "Fifth Object",
-            content: "This is the content of the fifth object."
-        },
-        {
-            id: 6,
-            title: "Sixth Object",
-            content: "This is the content of the sixth object."
-        },
-        {
-            id: 7,
-            title: "Seventh Object",
-            content: "This is the content of the seventh object."
-        },
-        {
-            id: 8,
-            title: "Eighth Object",
-            content: "This is the content of the eighth object."
-        },
-        {
-            id: 9,
-            title: "Ninth Object",
-            content: "This is the content of the ninth object."
-        },
-        {
-            id: 10,
-            title: "Tenth Object",
-            content: "This is the content of the tenth object."
-        }
-    ];
-    res.send(arrayOfObjects);
-})
-
 const PORT = process.env.PORT || 2008;
-
-app.listen(PORT, () => {
-    console.log(`server is running at port ${PORT}`)
+console.log(`${PORT}`);
+console.log(`${process.env.MONGODB_URL}`);
+app.get("/api/admin/test", (req, res) => {
+  const arrayOfObjects = [
+    {
+      id: 1,
+      title: "First Object",
+      content: "This is the content of the first object.",
+    },
+    {
+      id: 2,
+      title: "Second Object",
+      content: "This is the content of the second object.",
+    },
+    {
+      id: 3,
+      title: "Third Object",
+      content: "This is the content of the third object.",
+    },
+    {
+      id: 4,
+      title: "Fourth Object",
+      content: "This is the content of the fourth object.",
+    },
+    {
+      id: 5,
+      title: "Fifth Object",
+      content: "This is the content of the fifth object.",
+    },
+    {
+      id: 6,
+      title: "Sixth Object",
+      content: "This is the content of the sixth object.",
+    },
+    {
+      id: 7,
+      title: "Seventh Object",
+      content: "This is the content of the seventh object.",
+    },
+    {
+      id: 8,
+      title: "Eighth Object",
+      content: "This is the content of the eighth object.",
+    },
+    {
+      id: 9,
+      title: "Ninth Object",
+      content: "This is the content of the ninth object.",
+    },
+    {
+      id: 10,
+      title: "Tenth Object",
+      content: "This is the content of the tenth object.",
+    },
+  ];
+  res.send(arrayOfObjects);
 });
 
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
+
+//connecting to database;
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR, express failed to connect to Database");
+//       throw error;
+//     });
+//     app.listen(PORT, () => {
+//       console.log(`App is listening on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error;
+//   }
+// })();
